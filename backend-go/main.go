6275d9dc6d3703695595e1c62d301a00f22bbbf6
Alex <tabell@users.noreply.github.com>
2025-06-27 09:00:50 -0700

    Add selection adjuster and single-click word selection

--- backend-go/main.go
+++ backend-go/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
 )
 
 type PingResponse struct {
@@ -14,6 +15,11 @@ type LookupResponse struct {
 	Translation string `json:"translation"`
 	Knowledge   string `json:"knowledge"`
 }
+
+var (
+	contents []string
+	mtx      sync.Mutex
+)
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	res := PingResponse{Message: "pong"}
@@ -32,7 +38,19 @@ type ImportResponse struct {
 }
 
 func importHandler(w http.ResponseWriter, r *http.Request) {
-	res := ImportResponse{Status: "not implemented"}
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	text := r.FormValue("text")
+	if text == "" {
+		http.Error(w, "missing text", http.StatusBadRequest)
+		return
+	}
+	mtx.Lock()
+	contents = append(contents, text)
+	mtx.Unlock()
+	res := ImportResponse{Status: "ok"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(res)
 }
@@ -42,16 +60,26 @@ type ContentResponse struct {
 }
 
 func contentHandler(w http.ResponseWriter, r *http.Request) {
-	res := ContentResponse{Data: "not implemented"}
+	mtx.Lock()
+	var text string
+	if len(contents) > 0 {
+		text = contents[0]
+	}
+	mtx.Unlock()
+	res := ContentResponse{Data: text}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(res)
 }
 
 func main() {
+	contents = append(contents, "Hello World! This is sample content.")
+
 	http.HandleFunc("/ping", pingHandler)
 	http.HandleFunc("/lookup", lookupHandler)
 	http.HandleFunc("/import", importHandler)
 	http.HandleFunc("/content", contentHandler)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 
 	log.Println("Listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
